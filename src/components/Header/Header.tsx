360b96ae006a53600aad693a39f9f7e17fb3fb43
Shaulaev <mediggggh@gmai.com>
2022-11-08 02:45:41 +0300

    Readme

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import logo from ""
 import cl from "./Header.module.scss"
 
 export const Header: React.FC = () => {
   return (
     <header className={cl.header}>
         <div className={cl.container}>
-            <div className={cl.logo}></div>
+            <div className={cl.logo}>
+                <img src={} alt="" />
+            </div>
             <div className={cl.cart}></div>
         </div>
     </header>
